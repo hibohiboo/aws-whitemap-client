990fff4800e273d39c9ae4614fe4d7f86e3a5c2e
hibohiboo <swdice66@gmail.com>
2021-08-29 20:25:23 +0900

    fix 8 9互換の書き方に修正 AB#117

--- client/src/domain/firebase/index.ts
+++ client/src/domain/firebase/index.ts
@@ -1,6 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 import config from './config';
 import { TimeStamp } from './types';
 
